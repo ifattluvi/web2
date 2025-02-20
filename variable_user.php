7c419e75460acedfb9b6a86cf8a60ff5ce25b71b
ifattluvi <iffatluthfiyah05@gmail.com>
2025-02-20 12:16:38 +0700

    praktikum 1

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+//definisikan variables 
+$nama = 'Iffat';
+$umur = 19;
+$berat = 50.00;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>umur : ' . $umur . 'Tahun';
+echo '<br/>berat : ' . $berat . 'kg';
+
+?>
